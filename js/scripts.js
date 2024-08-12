9598b5d59b945e3de49bf4c8c0f99a3064bb3913
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-12 15:55:57 -0300

    estou fazendo os ajustes finais pra deixar ele bem organizado .

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
-/* Elemento */
-const notesContainer =document.querySelector("#notes-container");
+// Elementos
+const notesContainer = document.querySelector("#notes-container");
+const noteInput = document.querySelector("#note-content");
+const addNoteBtn = document.querySelector(".add-note");
 
-const noteInput =document.querySelector("note-content");
+// Funções
+function addNote() {
+    const notes =[];
 
-const addnoteBtn = document.querySelector(".add-note");
-
-// funções //
-function addNote(){
-    const noteObject ={
-        id:1,
-        content:noteInput.value,
-        fixed:false,
+    const noteObject = {
+        id: generateID(),
+        content: noteInput.value,
+        fixed: false,
     };
 
+    const noteElement = createNote(noteObject.id, noteObject.content);
+    notesContainer.appendChild(noteElement); 
     console.log(noteObject);
 }
 
-function generateID(){
-    return  Math.floor(Math.random() *5000);
+function generateID() {
+    return Math.floor(Math.random() * 5000);
 }
 
-// eventos //
-addnoteBtn.addEventListener("click",() =>addNote());
+function createNote(id, content) {
+    const noteDiv = document.createElement("div");
+    noteDiv.classList.add("note");
+    noteDiv.setAttribute("data-id", id);
+
+    const noteContent = document.createElement("p");
+    noteContent.textContent = content;
+    noteDiv.appendChild(noteContent);
+
+    return noteDiv;
+}
+function saveNotes(notes){
+    localStorage.setItem("notes",JSON.stringify(notes))
+}
+
+// Eventos//
+addNoteBtn.addEventListener("click", () => addNote());
